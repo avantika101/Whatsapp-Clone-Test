2a1189105a93ebefcf7806fd97c277cbbd7b0116
AVAPANDE <AVAPANDE@AVAPANDE-7410.us.oracle.com>
2021-03-13 16:04:55 +0530

    working on ui

---
+++ whatsapp-clone/src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBCx44YHGPe0f4hX1C0ui1h5PltBa3CZx8",
+    authDomain: "whatsapp-clone-test-87aca.firebaseapp.com",
+    projectId: "whatsapp-clone-test-87aca",
+    storageBucket: "whatsapp-clone-test-87aca.appspot.com",
+    messagingSenderId: "321724811002",
+    appId: "1:321724811002:web:fa210e5c43fc357af22b43"
+  };
