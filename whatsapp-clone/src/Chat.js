ff775cae9b68a52e1d6e4957c919ce0eb39e6ee0
AVAPANDE <AVAPANDE@AVAPANDE-7410.us.oracle.com>
2021-03-14 12:13:14 +0530

    adding chat header

---
+++ whatsapp-clone/src/Chat.js
@@ -0,0 +1,47 @@
+import React, { useState, useEffect } from 'react';
+import "./Chat.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from '@material-ui/icons/DonutLarge';
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+
+function Chat() {
+    const [seed, setSeed] = useState("")
+
+    useEffect(() => {
+        setSeed(Math.floor(Math.random()*5000));
+    }, [])
+
+    return (
+        <div className="chat">
+            <div className="chat__header">
+                <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+
+                <div className="chat__headerInfo">
+                    <h3>Room name</h3>
+                    <p>last seen at...</p>
+                </div>
+
+                <div className="chat__headerRight">
+                <IconButton>
+                        <DonutLargeIcon />
+                    </IconButton>
+                    <IconButton>
+                        <ChatIcon />
+                    </IconButton>
+                    <IconButton>
+                        <MoreVertIcon />
+                    </IconButton>
+                </div>
+            </div>
+            <div className="chat__body">
+
+            </div>
+            <div className="chat__footer">
+
+            </div>
+        </div>
+    )
+}
+
+export default Chat
