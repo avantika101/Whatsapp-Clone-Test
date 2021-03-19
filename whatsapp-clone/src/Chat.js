6f717a8dd13ab6e94b1c936df8bd586639616228
AVAPANDE <AVAPANDE@AVAPANDE.us.oracle.com>
2021-03-19 14:43:05 +0530

    configuring firebase

--- whatsapp-clone/src/Chat.js
+++ whatsapp-clone/src/Chat.js
@@ -18,6 +18,7 @@ function Chat() {
     const sendMessage = (e) => {
         e.preventDefault();
         console.log("you typed >>>", input);
+        setInput("");
     }
 
     return (
--- whatsapp-clone/src/firebase.js
+++ whatsapp-clone/src/firebase.js
@@ -1,8 +1,21 @@
+import firebase from 'firebase';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyBCx44YHGPe0f4hX1C0ui1h5PltBa3CZx8",
-    authDomain: "whatsapp-clone-test-87aca.firebaseapp.com",
-    projectId: "whatsapp-clone-test-87aca",
-    storageBucket: "whatsapp-clone-test-87aca.appspot.com",
-    messagingSenderId: "321724811002",
-    appId: "1:321724811002:web:fa210e5c43fc357af22b43"
-  };
+  apiKey: "AIzaSyBCx44YHGPe0f4hX1C0ui1h5PltBa3CZx8",
+  authDomain: "whatsapp-clone-test-87aca.firebaseapp.com",
+  databaseURL: "https://whatsapp-clone-test-87aca-default-rtdb.firebaseio.com",
+  projectId: "whatsapp-clone-test-87aca",
+  storageBucket: "whatsapp-clone-test-87aca.appspot.com",
+  messagingSenderId: "321724811002",
+  appId: "1:321724811002:web:fa210e5c43fc357af22b43"
+};
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+  const db = firebaseApp.firestore();
+  const auth = firebaseApp.auth();
+  const provider = new firebase.auth.GoogleAuthProvider();
+
+  export { auth, provider };
+  export default db;
+
+  
