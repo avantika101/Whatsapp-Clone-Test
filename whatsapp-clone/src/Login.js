65f3eddf2392f21b602b41fbce588aaee9c3d8b7
AVAPANDE <AVAPANDE@AVAPANDE.us.oracle.com>
2021-03-20 17:06:25 +0530

    auth

--- whatsapp-clone/src/Login.js
+++ whatsapp-clone/src/Login.js
@@ -1,9 +1,15 @@
 import { Button } from '@material-ui/core';
 import React from 'react';
+import { auth, provider } from './firebase';
 import "./Login.css";
 
 function Login() {
-    const signIn = () => {};
+    const signIn = () => {
+        auth
+            .signInWithPopup(provider)
+            .then((result) => console.log(result))
+            .catch((error) => alert(error.message));
+    };
     return (
         <div className="login">
             <div className="login__container">
