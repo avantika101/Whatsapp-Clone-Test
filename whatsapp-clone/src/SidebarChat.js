e0976a689c996dc6d730b2ace2cb0829a7cabeb9
AVAPANDE <AVAPANDE@AVAPANDE.us.oracle.com>
2021-03-25 23:15:30 +0530

    adding 'add new chat'

--- whatsapp-clone/src/SidebarChat.js
+++ whatsapp-clone/src/SidebarChat.js
@@ -2,24 +2,38 @@ import { Avatar } from '@material-ui/core';
 import React, { useEffect, useState } from 'react';
 import './SidebarChat.css';
 import { Link } from "react-router-dom";
+import db from './firebase';
 
 function SidebarChat({ addNewChat, name, id }) {
 
     const [seed, setSeed] = useState("");
     console.log(name);
     console.log(id);
+    const [messages, setMessages] = useState([]);
 
     useEffect(() => {
         setSeed(Math.floor(Math.random()*5000));
         console.log('hey there from useEffect')
     }, [])
 
+    useEffect(() => {
+        if(id) {
+            db.collection("rooms").doc(id).collection("messages").orderBy('timestamp','desc').onSnapshot(snapshot => (
+                setMessages(snapshot.docs.map((doc) => (
+                    doc.data()
+                )))
+            ));
+        }
+    }, [id]);
+
 
     const createChat = () => {
         const roomName = prompt("Please enter room name");
         console.log('roomname is',roomName);
         if (roomName) {
-            //do something
+            db.collection("rooms").add({
+                name: roomName
+            })
         }
     };
 
@@ -30,7 +44,7 @@ function SidebarChat({ addNewChat, name, id }) {
                 <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
                 <div className="sidebarChat__info">
                     <h2>{name}</h2>
-                    <p>Last message...</p>
+                    <p>{messages[0]?.message}</p>
                 </div>
             </div>
         </Link>
